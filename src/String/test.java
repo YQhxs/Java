1fffa72c28cde314b32d8e9c011d0c8f2a561efe
yqhxs <2325751238@qq.com>
2020-01-09 11:45:24 +0800

    IDE 测试冲突

--- src/String/test.java
+++ src/String/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         String s;
         s = new String("test");
-        s=s.concat("concat");
+        s = s.concat("git conflict");
         System.out.println(s);
     }
 }
